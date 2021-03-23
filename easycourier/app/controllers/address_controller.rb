676914d514062ee5e0c44a67428b6be1aea351cb
Jamal <jamensky1991@gmail.com>
2021-03-23 00:03:02 +0000

    created edit view and relevant controllers

--- easycourier/app/controllers/address_controller.rb
+++ easycourier/app/controllers/address_controller.rb
@@ -1,8 +1,9 @@
 class AddressController < ApplicationController
   
   get '/addresses' do 
-    all_addresses = Address.all 
-      if all_addresses
+    @all_addresses = Address.all 
+    #binding.pry 
+      if @all_addresses.empty? 
         erb :'addresses/error'
       else 
         erb :'addresses/index'
@@ -10,11 +11,41 @@ class AddressController < ApplicationController
   end 
 
   get '/addresses/new' do 
-    get :'addresses/new'
+    erb :'addresses/new'
   end 
 
   post '/addresses/new' do 
-    binding.pry 
+    @address = Address.create(params)
+    erb :'addresses/show'
+  end 
+
+  get '/addresses/:id' do
+    @address = Address.find_by_id(params[:id])
+    erb :'addresses/show'
+  end
+
+  get '/addresses/:id/edit' do 
+     @address = Address.find_by_id(params[:id])
+     erb :'addresses/edit'
+  end 
+
+  patch '/addresses/:id' do 
+     @address_to_edit = Address.find_by_id(params[:id])
+       @address_to_edit.client_name = params[:client_name]
+       @address_to_edit.first_line_of_address = params[:first_line_of_address]
+       @address_to_edit.post_code = params[:post_code]
+       @address_to_edit.contact = params[:contact]
+       @address_to_edit.reference = params[:reference]
+       @address_to_edit.note = params[:note]
+
+    @address_to_edit.save
+    
+    redirect "/addresses/#{params[:id]}"
+  end
+
+  delete 'addresses/:id' do 
+     @address = Address.find_by_id(params[:id])
+     #@address_to_delete.clear
   end 
 
 end 
